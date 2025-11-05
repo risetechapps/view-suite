e856738c54849b4ff945f253a9515e4010649d50
risetechapps <apps@risetech.com.br>
2025-11-05 00:06:37 -0300

    Atualizado template

--- resources/views/emails/default/index.blade.php
+++ resources/views/emails/default/index.blade.php
@@ -1,66 +1,55 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Document</title>
-</head>
-<body>
-EMAIL
-</body>
-</html>
-{{--@extends('emails.themes.default.layout')--}}
+@extends('view-suite::emails.default.layout')
 
-{{--@section('content')--}}
+@section('content')
 
-{{--    @include('emails.themes.default.header_subject', [--}}
-{{--    'headerSubject' => $subject_message--}}
-{{--    ])--}}
+    @include('view-suite::emails.default.header_subject', [
+    'headerSubject' => $subject_message
+    ])
 
-{{--    <div align="center" class="img-container center autowidth" style="padding-right: 0;padding-left: 0;">--}}
-{{--        <img align="center" alt="Image" border="0" class="center autowidth" src="https://i.imgur.com/ADTG9En.png"--}}
-{{--             style="text-decoration: none; -ms-interpolation-mode: bicubic; height: auto; border: 0; width: 100%;--}}
-{{--            max-width: 158px; display: block;" title="Image" width="158"/>--}}
-{{--    </div>--}}
+    <div align="center" class="img-container center autowidth" style="padding-right: 0;padding-left: 0;">
+        <img align="center" alt="Image" border="0" class="center autowidth" src="https://i.imgur.com/ADTG9En.png"
+             style="text-decoration: none; -ms-interpolation-mode: bicubic; height: auto; border: 0; width: 100%;
+            max-width: 158px; display: block;" title="Image" width="158"/>
+    </div>
 
-{{--    @isset($line)--}}
-{{--        @include('emails.themes.default.message_line', ['lineMessage' => $line])--}}
-{{--    @endisset--}}
+    @isset($line)
+        @include('view-suite::emails.default.message_line', ['lineMessage' => $line])
+    @endisset
 
-{{--    @isset($line_header)--}}
-{{--        @foreach($line_header as $line)--}}
-{{--            @include('emails.themes.default.message_line', ['lineMessage' => $line])--}}
-{{--        @endforeach--}}
-{{--    @endisset--}}
+    @isset($line_header)
+        @foreach($line_header as $line)
+            @include('view-suite::emails.default.message_line', ['lineMessage' => $line])
+        @endforeach
+    @endisset
 
-{{--    @if(!empty($tables))--}}
-{{--        @foreach($tables as $table)--}}
-{{--            @include('emails.themes.default.table', ['table' => $table])--}}
-{{--        @endforeach--}}
-{{--    @endif--}}
+    @if(!empty($tables))
+        @foreach($tables as $table)
+            @include('view-suite::emails.default.table', ['table' => $table])
+        @endforeach
+    @endif
 
-{{--    @if(!empty($lists))--}}
-{{--        @foreach($lists as $list)--}}
-{{--            @include('emails.themes.default.list', ['list' => $list])--}}
-{{--        @endforeach--}}
-{{--    @endif--}}
+    @if(!empty($lists))
+        @foreach($lists as $list)
+            @include('view-suite::emails.default.list', ['list' => $list])
+        @endforeach
+    @endif
 
-{{--    @isset($action)--}}
-{{--        @include('emails.themes.default.button', ['urlButton' => $action['url'], 'textButton' => $action['text']])--}}
-{{--    @endif--}}
+    @isset($action)
+        @include('view-suite::emails.default.button', ['urlButton' => $action['url'], 'textButton' => $action['text']])
+    @endif
 
-{{--    @isset($line_footer)--}}
-{{--        @foreach($line_footer as $line)--}}
-{{--            @include('emails.themes.default.message_line', ['lineMessage' => $line])--}}
-{{--        @endforeach--}}
-{{--    @endisset--}}
+    @isset($line_footer)
+        @foreach($line_footer as $line)
+            @include('view-suite::emails.default.message_line', ['lineMessage' => $line])
+        @endforeach
+    @endisset
 
-{{--    @isset($signature)--}}
-{{--        @if(is_array($signature))--}}
-{{--            @foreach($signature as $s)--}}
-{{--                @include('emails.themes.default.signature', ['signature' => $s])--}}
-{{--            @endforeach--}}
-{{--        @endif--}}
-{{--    @endisset--}}
+    @isset($signature)
+        @if(is_array($signature))
+            @foreach($signature as $s)
+                @include('view-suite::emails.default.signature', ['signature' => $s])
+            @endforeach
+        @endif
+    @endisset
 
-{{--@endsection--}}
+@endsection
